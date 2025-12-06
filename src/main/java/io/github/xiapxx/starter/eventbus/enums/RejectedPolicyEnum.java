f5aece29ad0d48f62968b8dcb7dd4dd3a3efe8cc
xiapxx <250891957@qq.com>
2025-12-06 21:16:40 +0800

    xx

--- src/main/java/io/github/xiapxx/starter/eventbus/enums/RejectedPolicyEnum.java
+++ src/main/java/io/github/xiapxx/starter/eventbus/enums/RejectedPolicyEnum.java
@@ -2,6 +2,8 @@ package io.github.xiapxx.starter.eventbus.enums;
 
 /**
  * 拒绝策略枚举
+ * 1. eventBusPublisher.*Parallel类型事件只支持CALLER_RUNS
+ * 2. 可通过在onEvent方法中调用EventRejectMonitor.isRejected()方法判断是否发生了溢出
  *
  * @Author xiapeng
  * @Date 2025-04-10 14:46
